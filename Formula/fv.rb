ccd0617f106aa640559404e7860924ad40ee1bc2
Kenneth Shaw <kenshaw@gmail.com>
2023-11-10 13:10:09 +0700

    Initial commit

---
+++ Formula/fv.rb
@@ -0,0 +1,28 @@
+$pkg     = "github.com/kenshaw/fv"
+
+class Fv < Formula
+  desc "a command-line font viewer using terminal graphics (Sixel, iTerm, Kitty)"
+  homepage "https://#{$pkg}"
+  head "https://#{$pkg}.git"
+  url "https://github.com/kenshaw/fv/archive/v0.4.4.tar.gz"
+  sha256 "9566052458c02df4b01ed87bfac20d906417cd2447fde3842c1299252066d759"
+
+  depends_on "go" => :build
+
+  def install
+    (buildpath/"src/#{$pkg}").install buildpath.children
+
+    cd "src/#{$pkg}" do
+      system "go", "mod", "download"
+      system "go", "build",
+        "-trimpath",
+        "-ldflags", "-s -w -X main.version=#{self.version}",
+        "-o",       bin/"fv"
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/fv --version")
+    assert_match "fv #{self.version}", output
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# About homebrew-fv
+
+Provides Homebrew formulas for [fv](https://github.com/kenshaw/fv),
+
+## Installing
+
+Install and use in the normal way with Homebrew:
+
+```sh
+# install
+$ brew install kenshaw/fv/fv
+```
+
+## Uninstalling
+
+Uninstall in the usual way with Homebrew:
+
+```sh
+# uninstall
+$ brew uninstall kenshaw/fv/fv
+```
