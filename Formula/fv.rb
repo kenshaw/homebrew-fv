9d6d41dd1188027a9a44c9193fb10902e30bd271
Kenneth Shaw <kenshaw@gmail.com>
2024-07-25 05:00:41 +0000

    Update fv version to v0.5.2
    **Full Changelog**: https://github.com/kenshaw/fv/compare/v0.5.1...v0.5.2

--- Formula/fv.rb
+++ Formula/fv.rb
@@ -4,8 +4,8 @@ class Fv < Formula
   desc "a command-line font viewer using terminal graphics (Sixel, iTerm, Kitty)"
   homepage "https://#{$pkg}"
   head "https://#{$pkg}.git"
-  url "https://github.com/kenshaw/fv/archive/v0.5.1.tar.gz"
-  sha256 "5ad41d61946cfe287a9eb62cc47d9fa3f6dd0aa134e28e7dab8fbeaf0e7bd31a"
+  url "https://github.com/kenshaw/fv/archive/v0.5.2.tar.gz"
+  sha256 "b6bb3ac0f4e8a775ef0ee19e5c030d07a63df0921a5a04430c6e18d9e7e071ba"
 
   depends_on "go" => :build
 
